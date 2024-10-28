a3c79e17919dd3b51c8fcb77fb46ab5d8e5690ed
gaurav <gauravshirke895@gmail.com>
2024-10-28 11:44:31 +0530

    edited

--- server/util/db.js
+++ server/util/db.js
@@ -1,12 +1,10 @@
 const mongoose=require('mongoose');
 
-// const URI="mongodb+srv://gauravshirke895:8ppx1R6oqRDP4QXa@cluster0.evjjlcb.mongodb.net/mern_admin?retryWrites=true&w=majority&appName=Cluster0";
-const uRi="mongodb+srv://gauravsh:Gagosh29@cluster0.evjjlcb.mongodb.net/mern_admin?retryWrites=true&w=majority&appName=Cluster0"
-// const uri=process.env.MONGODB;
+const uri=process.env.MONGODB;
 const connectDB=async()=>
 {
     try{
-        await mongoose.connect(uRi);
+        await mongoose.connect(uri);
         console.log("connected to db ");
         
     }catch(err){
